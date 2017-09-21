9fd4c78b5fb9fdf33b4aa15c23de2c05b79e3098
coreyfurman <cfurm14@gmail.com>
2017-09-21 19:36:37 -0400

    capitalR

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
-require('./app/routing/apiroutes.js')(app); 
-require('./app/routing/htmlroutes.js')(app);
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
 
 app.listen(PORT, function() {
